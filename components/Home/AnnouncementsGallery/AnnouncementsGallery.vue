e2e002f416c75fb6107b1458d28a00cc3a84da81
josephjoeljo <joeljordanjoseph@gmail.com>
2025-01-10 20:52:50 -0500

    chore: cleaned up announcemnts css

--- components/Home/AnnouncementsGallery/AnnouncementsGallery.vue
+++ components/Home/AnnouncementsGallery/AnnouncementsGallery.vue
@@ -1,7 +1,38 @@
 <template>
-    <div class="container">
+    <div id="announcements-gallery">
         <h2>Announcements</h2>
-        <Galleria v-if="state !== VIEW_STATE.LOADING" :value="images" :showThumbnails="false" :showItemNavigators="true">
+        <Galleria 
+            v-if="state !== VIEW_STATE.LOADING" 
+            :value="images" 
+            :showThumbnails="false" 
+            :showItemNavigators="true"
+            :pt="{
+                nextButton: (options) => ({
+                    style: {
+                        'width': '3rem',
+                        'height': '3rem'
+                    }
+                }),
+                nextIcon: (options) => ({
+                    style: {
+                        'width': '2.5rem',
+                        'height': '2.5rem'
+                    }
+                }),
+                prevButton: (options) => ({
+                    style: {
+                        'width': '3rem',
+                        'height': '3rem'
+                    }
+                }),
+                prevIcon: (options) => ({
+                    style: {
+                        'width': '2.5rem',
+                        'height': '2.5rem'
+                    }
+                })
+            }"
+        >
             <template #item="slotProps">
                 <img :src="slotProps.item" class="image" />
             </template>
@@ -19,48 +50,51 @@ import Galleria from 'primevue/galleria';
 import { VIEW_STATE } from "~/data/Enums";
 import { useSessionStore } from "~/stores/session-store";
 
-    const sessionStore = useSessionStore()
+const sessionStore = useSessionStore()
 
-    const state = computed<VIEW_STATE>(() => {
-        return sessionStore.loadingState;
-    });
+const state = computed<VIEW_STATE>(() => {
+    return sessionStore.loadingState;
+});
 
-    const images = computed<string[]>(() => {
-        return sessionStore.announcements;
-    })
+const images = computed<string[]>(() => {
+    return sessionStore.announcements;
+})
 </script>
 
 <style scoped>
-    .container {
-        width: auto;
-        height: 100%;
-        max-height: 1000px;
+#announcements-gallery {
+    width: auto;
+    height: 100%;
+    max-height: 1000px;
+    * {
+        border: unset;
+    }
 
-        h2 {
-            margin-bottom: 1rem;
-            margin-left: 1rem;
-            color: var(--primary-label-color);
-            font-family: 'Helvetica Nue', 'Arial', 'Roboto', sans-serif;
-        } 
+    h2 {
+        margin-bottom: 1rem;
+        margin-left: 1rem;
+        color: var(--primary-label-color);
+        font-family: 'Helvetica Nue', 'Arial', 'Roboto', sans-serif;
+    } 
 
+    .image {
+        width: 100%;
+        height: auto;
+        border-radius: 10px;
+    }
+
+    @media (max-width: 940px) {
         .image {
             width: 100%;
             height: auto;
             border-radius: 10px;
         }
+    }
 
-        @media (max-width: 940px) {
-            .image {
-                width: 100%;
-                height: auto;
-                border-radius: 10px;
-            }
-        }
-
-        @media (max-width: 675px) {
-            .image {
-                border-radius: unset;
-            }
+    @media (max-width: 675px) {
+        .image {
+            border-radius: unset;
         }
     }
+}
 </style>

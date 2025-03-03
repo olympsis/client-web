132817db9fa88eb99cdcde482acc4c2801fe70da
josephjoeljo <joeljordanjoseph@gmail.com>
2025-03-03 18:52:05 -0500

    updated site list item

--- components/Venues/VenueListItem/VenueListItem.vue
+++ components/Venues/VenueListItem/VenueListItem.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="wrapper">
+    <div id="venue-list-item">
         <img :src="image" class="image" @click="visible = true"/>
         <div class="bottom">
             <div class="info">
-                <div class="location">
-                {{ city }}, {{ state }}
-                </div>
-                <div class="name">
-                    {{ name }}
+                <div class="name">{{ name }}</div>
+
+                <div class="location-wrapper">
+                    <picture>
+                        <source srcset="@/assets/icons/pin-drop/pin.drop.svg" media="(prefers-color-scheme: dark)"> 
+                        <img class="location-pin" src="@/assets/icons/pin-drop/pin.drop.svg"> 
+                    </picture>
+                    <div class="location">{{ city }}, {{ state }}</div>
                 </div>
             </div>
             
@@ -32,12 +35,12 @@
 </template>
 
 <script setup lang="ts">
-
 import { ref, computed } from 'vue';
-import Dialog from 'primevue/dialog';
-import { getDirections } from '~/utils/map-helpers';
 import { Venue } from '@/data/models/VenueModels';
+import { getDirections } from '~/utils/map-helpers';
 import { generateImageURL } from '~/utils/image-extensions';
+
+import Dialog from 'primevue/dialog';
 import VenueDetailCard from '@/components/Venues/VenueDetailCard/VenueDetailCard.vue';
 
 const props = defineProps({
@@ -79,46 +82,55 @@ const openMaps = () => {
 </script>
 
 <style scoped>
-.wrapper {
+#venue-list-item {
     margin: 0rem 1rem;
-    
+    border-radius: 0px 0px 10px 10px;
+    box-shadow: rgba(0, 0, 0, 0.16) 0px 1px 4px;
+    background-color: var(--secondary-background-color);
+
     .image {
         width: 100%;
         height: auto;
-        border-radius: 10px;
+        border-radius: 10px 10px 0px 0px;
     }
 
     .bottom {
         display: flex;
         align-items: end;
+        padding: 0.25rem 1rem 0.75rem 1rem;
         justify-content: space-between;
-        margin: 0.3rem 0rem;
+        
         
         .info {
             display: flex;
             flex-direction: column;
-            .location {
-                color: gray;
-            }
+            
             .name {
-                font-weight: 600;
-                font-size: 1.2rem;
-                font-style: normal;
                 margin: unset;
+                font-weight: 600;
+                font-size: 1.1rem;
+                font-style: normal;
                 color: var(--primary-label-color);
+            }
+
+            .location-wrapper {
+                display: flex;
+                margin-top: 0.25rem;
+            }
+
+            .location-pin {
+                width: 1rem;
+                height: 1rem;
+                margin-right: 0.25rem;
+            }
+
+            .location {
+                font-size: 0.9rem;
             }
         }
         .directions {
             width: 2rem;
             cursor: pointer;
-        }
-    }
-}
-
-@media (max-width: 940px) {
-    .wrapper {
-        .image {
-            border-radius: 10px;
         }
     }
 }

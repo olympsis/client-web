e8ddffe1de0e331317afab003786eb887b467958
josephjoeljo <joeljordanjoseph@gmail.com>
2025-03-11 15:50:52 -0400

    updated group menu

--- components/Modals/Groups/GroupSettings/GroupSettingsModal.vue
+++ components/Modals/Groups/GroupSettings/GroupSettingsModal.vue
@@ -12,11 +12,42 @@
             </div>
         </div>
         <div id="actions">
-            <button @click="router.push('/groups/reports')"> Applications/Reports </button>
-            <button @click="router.push('/groups/new')"> Create a New Group </button>
-            <button @click="router.push('/groups/search')"> Search for clubs </button>
-            <button v-if="canLeaveGroup" class="destructive" @click="$emit('show-leave-modal')"> Leave Club </button>
-            <button v-if="canDeleteGroup" class="destructive" @click="$emit('show-delete-modal')"> Delete Club </button>
+            <button @click="router.push('/groups/reports')">
+                <picture class="icon">
+                    <source srcset="@/assets/icons/description/description.fill.white.svg" media="(prefers-color-scheme: dark)">
+                    <img src="@/assets/icons/description/description.fill.svg">
+                </picture>
+                Applications/Reports
+            </button>
+
+            <button @click="router.push('/groups/new')">
+                <picture class="icon" :style="{ width: '24px' }">
+                    <source srcset="@/assets/icons/plus/plus.white.svg" media="(prefers-color-scheme: dark)">
+                    <img src="@/assets/icons/plus/plus.svg">
+                </picture>
+                Create a New Group
+            </button>
+
+            <button @click="router.push('/groups/search')">
+                <picture class="icon">
+                    <source srcset="@/assets/icons/search/search.white.svg" media="(prefers-color-scheme: dark)">
+                    <img src="@/assets/icons/search/search.svg">
+                </picture>
+                Search for clubs
+            </button>
+
+            <button v-if="canLeaveGroup" class="destructive" @click="$emit('show-leave-modal')">
+                <picture class="icon">
+                    <source srcset="@/assets/icons/door-open/door.open.fill.white.svg" media="(prefers-color-scheme: dark)">
+                    <img src="@/assets/icons/door-open/door.open.fill.svg">
+                </picture>
+                Leave Club 
+            </button>
+            
+            <button v-if="canDeleteGroup" class="destructive" @click="$emit('show-delete-modal')"> 
+                <img src="@/assets/icons/trash/trash.white.svg">
+                Delete Club
+            </button>
         </div>
     </div>
 </template>
@@ -83,23 +114,35 @@ const canDeleteGroup = computed<boolean>(() => {
         width: 100%;
         display: flex;
         max-width: 25rem;
-        padding: 0rem 1rem;
+        padding: 0rem 2rem;
         margin-bottom: 2rem;
         flex-direction: column;
 
-        * {
+        button {
             border: unset;
             padding: 1rem;
+            display: flex;
             cursor: pointer;
             font-size: 0.9rem;
             margin: 0.3rem 0rem;
+            align-items: center;
             color: var(--primary-label-color);
             border-radius: var(--button-border-radius);
             background-color: var(--tertiary-background-color);
         }
 
+        .icon {
+            /* width: 24px; */
+            height: 24px;
+            display: flex;
+            align-items: center;
+            margin-right: 0.5rem;
+        }
+
         .destructive {
+            display: flex;
             color: white;
+            align-items: center;
             background-color: #8c1d1d;
         }
     }

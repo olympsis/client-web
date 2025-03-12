f5eb32db9e7782875047f9f1816006f677acfa19
josephjoeljo <joeljordanjoseph@gmail.com>
2025-03-12 19:23:22 -0400

    i hope this fixes sentry damn

--- public/instrument.server.mjs
+++ public/instrument.server.mjs
@@ -2,7 +2,7 @@ import * as Sentry from '@sentry/nuxt';
 
 // Only run `init` when DSN is available
 if (process.env.SENTRY_DSN) {
-  Sentry.init({
-    dsn: process.env.DSN,
-  });
+    Sentry.init({
+        dsn: process.env.DSN,
+    });
 }
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,12 +1,14 @@
 import * as Sentry from "@sentry/nuxt";
  
-Sentry.init({
-  dsn: "https://96871dab3c4734efce45ca357cc9aee4@o4508966665388032.ingest.us.sentry.io/4508966666764288",
+if (process.env.DSN) {
+	Sentry.init({
+		dsn: process.env.DSN,
 
-  // We recommend adjusting this value in production, or using tracesSampler
-  // for finer control
-  tracesSampleRate: 1.0,
-  
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
-});
+		// We recommend adjusting this value in production, or using tracesSampler
+		// for finer control
+		tracesSampleRate: 1.0,
+
+		// Setting this option to true will print useful information to the console while you're setting up Sentry.
+		debug: false,
+	});  
+}

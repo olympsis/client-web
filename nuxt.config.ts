19d55faad2fbe2056792abc0f3ff5dd86c896c9e
josephjoeljo <joeljordanjoseph@gmail.com>
2025-06-06 23:53:15 -0400

    seems stable enough to push to main i think

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,120 +5,120 @@ export default defineNuxtConfig({
  devtools: { enabled: true },
 
  css: [
-					'~/assets/css/main.css',
-					'~/assets/css/modal.css',
-					'~/assets/css/popup.css',
-					'~/assets/css/dialog.css',
-					'~/assets/css/animations.css'
-	],
+																				'~/assets/css/main.css',
+																				'~/assets/css/modal.css',
+																				'~/assets/css/popup.css',
+																				'~/assets/css/dialog.css',
+																				'~/assets/css/animations.css'
+				],
 
  modules: [
-	 '@pinia/nuxt',
-	 '@nuxtjs/robots',
-	 '@nuxtjs/storybook',
-	 '@primevue/nuxt-module',
-	 '@sentry/nuxt/module'
-	],
+					'@pinia/nuxt',
+					'@nuxtjs/robots',
+					'@nuxtjs/storybook',
+					'@primevue/nuxt-module',
+					'@sentry/nuxt/module'
+				],
 
  build: {
-					transpile: ['malakbel']
-	},
+																				transpile: ['malakbel']
+				},
 
  runtimeConfig: {
-					public: {
-									MODE: process.env.mode ?? 'dev',
-									
-									API: process.env.API ?? '',
+																				public: {
+																																				MODE: process.env.mode ?? 'dev',
+																																				
+																																				API: process.env.API ?? '',
 
-									APP_ID: process.env.APP_ID ?? '',
-									APL_KEY_ID: process.env.APL_KEY_ID ?? '',
-									APL_TEAM_ID: process.env.APL_TEAM_ID ?? '',
-									APL_MAPKIT_SNAPSHOT_TOKEN: process.env.APL_MAPKIT_SNAPSHOT_TOKEN ?? '',
+																																				APP_ID: process.env.APP_ID ?? '',
+																																				APL_KEY_ID: process.env.APL_KEY_ID ?? '',
+																																				APL_TEAM_ID: process.env.APL_TEAM_ID ?? '',
+																																				APL_MAPKIT_SNAPSHOT_TOKEN: process.env.APL_MAPKIT_SNAPSHOT_TOKEN ?? '',
 
-									MAPKIT_ORIGIN: process.env.MAPKIT_ORIGIN ?? '',
-									MAPKIT_KEY: process.env.MAPKIT_KEY ?? '',
+																																				MAPKIT_ORIGIN: process.env.MAPKIT_ORIGIN ?? '',
+																																				MAPKIT_KEY: process.env.MAPKIT_KEY ?? '',
 
-									FB_APP_ID: process.env.FB_APP_ID ?? '',
-									FB_API_KEY: process.env.FB_API_KEY ?? '',
-									FB_PROJECT_ID: process.env.FB_PROJECT_ID ?? '',
-									FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN ?? '',
-									FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET ?? '',
-									FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID ?? '',
-									FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID ?? ''
-					}
-	},
+																																				FB_APP_ID: process.env.FB_APP_ID ?? '',
+																																				FB_API_KEY: process.env.FB_API_KEY ?? '',
+																																				FB_PROJECT_ID: process.env.FB_PROJECT_ID ?? '',
+																																				FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN ?? '',
+																																				FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET ?? '',
+																																				FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID ?? '',
+																																				FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID ?? ''
+																				}
+				},
 
  routeRules: {
-					// Sites routes
-					'/': {
-									redirect: '/signin'
-					},
-					'/signin': { ssr: false },
-					'/about-us': { prerender: true },
-					'/contact-us': { prerender: true },
-					'/terms-of-use': { prerender: true },
-					'/privacy-policy': { prerender: true },
+																				// Sites routes
+																				'/': {
+																																				redirect: '/signin'
+																				},
+																				'/signin': { ssr: false },
+																				'/about-us': { prerender: true },
+																				'/contact-us': { prerender: true },
+																				'/terms-of-use': { prerender: true },
+																				'/privacy-policy': { prerender: true },
 
-					'/home': { ssr: false },
-					'/groups': { ssr: false },
-					'/groups/search': { ssr: false },
-					'/events': { ssr: false },
-					'/profile': { ssr: false },
+																				'/home': { ssr: false },
+																				'/groups': { ssr: false },
+																				'/groups/search': { ssr: false },
+																				'/events': { ssr: false },
+																				'/profile': { ssr: false },
 
-					'/events/**': { ssr: true },
-					'/groups/search/**': { ssr: true}
+																				'/events/**': { ssr: true },
+																				'/groups/search/**': { ssr: true}
 
-	},
+				},
 
  app: {
-					head: {
-									link: [
-													{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-									],
-									meta: [
-													{
-																	name: 'theme-color',
-																	content: '#FFFFFF',
-																	media: '(prefers-color-scheme: light)'
-															},
-															{
-																	name: 'theme-color',
-																	content: '#1E1E1E',
-																	media: '(prefers-color-scheme: dark)'
-															}
-									]
-					}
-	},
+																				head: {
+																																				link: [
+																																																				{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+																																				],
+																																				meta: [
+																																																				{
+																																																																				name: 'theme-color',
+																																																																				content: '#FFFFFF',
+																																																																				media: '(prefers-color-scheme: light)'
+																																																												},
+																																																												{
+																																																																				name: 'theme-color',
+																																																																				content: '#1E1E1E',
+																																																																				media: '(prefers-color-scheme: dark)'
+																																																												}
+																																				]
+																				}
+				},
 
  robots: {
-					blockAiBots: true,
-					blockNonSeoBots: true
-	},
+																				blockAiBots: true,
+																				blockNonSeoBots: true
+				},
 
-	primevue: {
-		options: {
-			theme: {
-				preset: Aura
-			}
-		},
-		components: {
-			prefix: 'Prime',
-			exclude: ['Editor', 'Form', 'Chart', 'FormField']
-		}
-	},
+				primevue: {
+								options: {
+												theme: {
+																preset: Aura
+												}
+								},
+								components: {
+												prefix: 'Prime',
+												exclude: ['Editor', 'Form', 'Chart', 'FormField']
+								}
+				},
 
  spaLoadingTemplate: 'loading.html',
 
  sentry: {
-	 sourceMapsUploadOptions: {
-		 org: 'olympsis',
-		 project: 'client-web'
-		},
+					sourceMapsUploadOptions: {
+									org: 'olympsis',
+									project: 'client-web'
+								},
 
-	 autoInjectServerSentry: 'top-level-import'
-	},
+					autoInjectServerSentry: 'top-level-import'
+				},
 
  sourcemap: {
-	 client: 'hidden'
-	}
+					client: 'hidden'
+				}
 });
--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,14 +1,12 @@
 import * as Sentry from "@sentry/nuxt";
  
-if (process.env.DSN) {
-	Sentry.init({
-		dsn: process.env.DSN,
+Sentry.init({
+  dsn: "https://96871dab3c4734efce45ca357cc9aee4@o4508966665388032.ingest.us.sentry.io/4508966666764288",
 
-		// We recommend adjusting this value in production, or using tracesSampler
-		// for finer control
-		tracesSampleRate: 1.0,
-
-		// Setting this option to true will print useful information to the console while you're setting up Sentry.
-		debug: false,
-	});  
-}
+  // We recommend adjusting this value in production, or using tracesSampler
+  // for finer control
+  tracesSampleRate: 1.0,
+  
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
